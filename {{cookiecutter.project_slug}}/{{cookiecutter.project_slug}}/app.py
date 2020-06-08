d7d8c4493250654a4ee3badb36c4c4da1ccb8d3d
VaultVulp <me@vaultvulp.dev>
2020-06-09 01:33:06 +0300

    Reword messages for app state

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
@@ -8,9 +8,10 @@ from {{cookiecutter.project_slug}}.logger import setup_logger
 def main():
     try:
         asyncio.run(setup_logger())
+        logger.info('Starting {{cookiecutter.project_name}} ...')
         asyncio.run(do_work())
     except (KeyboardInterrupt, SystemExit):
-        logger.info('Closing the app')
+        logger.info('Closing {{cookiecutter.project_name}}')
     except Exception:
         logger.exception('Critical Error!')
 
